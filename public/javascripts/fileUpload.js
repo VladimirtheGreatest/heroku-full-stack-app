c67126694fd6c1fc344b11bb47e3568177b1b316
VladimirtheGreatest <vladkodoktorov@gmail.com>
2019-07-24 18:09:43 +0100

    css styles final commit

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,17 +1,28 @@
 //A JavaScript library that can upload anything you throw at it, optimizes images for faster uploads, and offers a great, accessible, silky smooth user experience.
 //will be used to upload book covers, this will replace the dependencie multer since we no longer need to upload files but we upload json object as a string to the database
-FilePond.registerPlugin(
-   FilePondPluginImagePreview,
-   FilePondPluginImageResize,
-   FilePondPluginFileEncode,
- )
+const rootStyles = window.getComputedStyle(document.documentElement)
 
- FilePond.parse(document.body);
+if (rootStyles.getPropertyValue('--book-cover-width-large') != null && rootStyles.getPropertyValue('--book-cover-width-large') !== '') {
+  ready()
+} else {
+  document.getElementById('main-css').addEventListener('load', ready)
+}
 
+function ready() {
+  const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-width-large'))
+  const coverAspectRatio = parseFloat(rootStyles.getPropertyValue('--book-cover-aspect-ratio'))
+  const coverHeight = coverWidth / coverAspectRatio
+  FilePond.registerPlugin(
+    FilePondPluginImagePreview,
+    FilePondPluginImageResize,
+    FilePondPluginFileEncode,
+  )
 
-//width and height of the image will be adjusted
- FilePond.setOptions({
-  stylePanelAspectRatio: 150 / 100,
-  imageResizeTargetWidth: 100,
-  imageResizeTargetHeight: 150
-})
+  FilePond.setOptions({
+    stylePanelAspectRatio: 1 / coverAspectRatio,
+    imageResizeTargetWidth: coverWidth,
+    imageResizeTargetHeight: coverHeight
+  })
+
+  FilePond.parse(document.body)
+}
